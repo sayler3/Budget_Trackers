bf87be47ee9aaab1c979a145457f8f45a665a39d
Sam Ayler <sayler8701@gmail.com>
2021-02-17 07:50:19 -0800

    add to cache assets

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,13 @@
 const staticCache = "site-static-v1";
 
-const assets = ["./", "./index.html", "./index.js", "./styles.css"];
+const assets = [
+	"./",
+	"./index.html",
+	"./js/index.js",
+	"./styles.css",
+	"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+	"https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 
 //install event
 self.addEventListener("install", (evt) => {
